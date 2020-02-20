125cd7583c32f84221a3a32b3cbff423694272a1
rj7shakya <shakyarajad1@gmail.com>
2020-02-20 09:32:47 +0545

    a test

--- rajad_shakya/js/dropdown.js
+++ rajad_shakya/js/dropdown.js
@@ -5,22 +5,52 @@ var tablet = document.getElementById("tablet");
 var mobile_d = document.getElementById("mobiled");
 var pc_d = document.getElementById("pcd");
 var tab_d = document.getElementById("tabd");
-pc_d.style.display = 'None';
-mobile_d.style.display = 'None';
-tab_d.style.display = 'None';
+// pc_d.style.display = 'None';
+// mobile_d.style.display = 'None';
+// tab_d.style.display = 'None';
 
-setInterval(function(){
-        if(mobile.checked){
-            pc_d.style.display = 'None';
-            mobile_d.style.display = 'block';
-            tab_d.style.display = 'None';
-        }else if(pc.checked){
-            mobile_d.style.display = 'None';
-            pc_d.style.display = 'block';
-            tab_d.style.display = 'None';
-        }else if(tablet.checked){
-            mobile_d.style.display = 'None';
-            pc_d.style.display = 'None';
-            tab_d.style.display = 'Block';
-        } 
-    }, 300);
+mobileArray = [];
+// mobileArray = ['mobile operating system used.',
+//                 'Android','iOS'];
+// pcArray = ['pc/laptop operating system used.',
+//             'Windows','linux'];
+tabletArray = [];
+// var select = document.getElementById("select");
+
+// setInterval(function(){
+//         if(mobile.checked){
+//             pc_d.style.display = 'None';
+//             mobile_d.style.display = 'block';
+//             tab_d.style.display = 'None';
+//         }else if(pc.checked){
+//             mobile_d.style.display = 'None';
+//             pc_d.style.display = 'block';
+//             tab_d.style.display = 'None';
+//         }else if(tablet.checked){
+//             mobile_d.style.display = 'None';
+//             pc_d.style.display = 'None';
+//             tab_d.style.display = 'Block';
+//         } 
+    
+    // }, 300);
+
+function change(){
+    mobileArray.forEach(function(element,index){
+        var option = document.createElement("OPTION"),
+        txt = document.createTextNode(element);
+        option.appendChild(txt);
+        option.setAttribute("value",element);
+        mobile_d.insertBefore(option,mobile_d.lastChild);
+    });
+    if(mobile.checked){
+        txt = document.getElementsByTagName('OPTION');
+        console.log(txt);
+        mobileArray = ['mobile operating system used.',
+                'Android','iOS'];
+    }else if(pc.checked){
+        mobileArray = ['pc/laptop operating system used.',
+            'Windows','linux'];
+    }
+    
+    // mobileArray = [];
+}
