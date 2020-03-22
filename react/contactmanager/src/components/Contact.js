fe5bdb117f005f15419897b506a885eb8f4b5750
rj7shakya <shakyarajad1@gmail.com>
2020-03-22 19:00:36 +0545

    first app

--- react/contactmanager/src/components/Contact.js
+++ react/contactmanager/src/components/Contact.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 class Contact extends Component {
-  render() {
+  static propTypes = {
+      name : PropTypes.string.isRequired,
+      email : PropTypes.string.isRequired,
+      phone : PropTypes.string.isRequired,
+    }
+    render() {
+    
+    const {name,email,phone} = this.props;
     return (
       <div>
-        <h4>{this.props.name}</h4>
+        <h4>{name}</h4>
         <ul>
-          <li>Email : {this.props.email}</li>
-          <li>Phone : {this.props.phone}</li>
+          <li>Email : {email}</li>
+          <li>Phone : {phone}</li>
         </ul>
       </div>
     )
   }
 }
+
+// Contact.propTypes = {
+//   name : PropTypes.string.isRequired,
+//   email : PropTypes.string.isRequired,
+//   phone : PropTypes.string.isRequired,
+// }
 
 export default Contact;
