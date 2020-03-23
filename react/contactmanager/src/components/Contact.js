33b40fa0b951a576cb407961e552c3f0ca346103
rj7shakya <shakyarajad1@gmail.com>
2020-03-23 14:57:41 +0545

    first app

--- react/contactmanager/src/components/Contact.js
+++ react/contactmanager/src/components/Contact.js
@@ -8,7 +8,8 @@ class Contact extends Component {
     render() { 
     
     const {name,email,phone} = this.props.contact;
-    
+    const {showContactInfo} = this.state;
+
     return (
       <div className="card card-body mb-3"> 
         <h4>{name} <i onClick={
@@ -16,10 +17,13 @@ class Contact extends Component {
             this.setState({showContactInfo:!this.state.showContactInfo});
           }
         } className="fas fa-sort-down"></i> </h4>
-        <ul className="list-group">
+        {showContactInfo?(
+          <ul className="list-group">
           <li className="list-group-item">Email : {email}</li>
           <li className="list-group-item">Phone : {phone}</li>
         </ul>
+        ):null}
+        
       </div>
     )
   }
