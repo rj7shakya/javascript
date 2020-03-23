dd7c6c9502a6c1c1e410074d5a5412e7d8397d57
rj7shakya <shakyarajad1@gmail.com>
2020-03-23 17:45:17 +0545

    first app

--- react/contactmanager/src/components/Contacts.js
+++ react/contactmanager/src/components/Contacts.js
@@ -1,53 +1,29 @@
 import React, { Component } from 'react'
 import Contact from './Contact'
-
+import { Consumer } from '../context'
 
 class Contacts extends Component {
-  state={
-      contacts:[
-        {
-          id: 1,
-          name: 'John Doe',
-          email: 'jdoe@gmail.com',
-          phone: '555-555-5555'
-        },
-        {
-          id: 2,
-          name: 'Karen Williams',
-          email: 'karen@gmail.com',
-          phone: '555-455-5555'
-        },
-        {
-          id: 3,
-          name: 'Henry Johnson',
-          email: 'henry@gmail.com',
-          phone: '555-545-5555'
-        },
-      ]
-    };
-    deleteContact = (id)=>{
-      const {contacts} = this.state;
-      const newContacts = contacts.filter(contact => contact.id !== id);
-      this.setState({
-        contacts : newContacts
-      });
-    }
-
   
-  render() {
-    const {contacts} = this.state;
 
-    return (
-      <React.Fragment>
-        {contacts.map(contact=>(
-            <Contact
-              key={contact.id}
-              contact={contact}
-              deleteClickHandler={this.deleteContact.bind(this,contact.id)}
-            />
-        ))}
-      </React.Fragment>
-    );
+  render(){
+    return(
+      <Consumer>
+        {value=>{
+          const {contacts} = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact=>(
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }
+        }
+      </Consumer>
+    )
   }
 }
 export default Contacts;
