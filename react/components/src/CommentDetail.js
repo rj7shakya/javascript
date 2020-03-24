4c10bdcddcd855e4a04e0fabac51b1c3edc187a0
rj7shakya <shakyarajad1@gmail.com>
2020-03-24 14:03:20 +0545

    component

--- react/components/src/CommentDetail.js
+++ react/components/src/CommentDetail.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+import React from 'react'
 
-const App = ()=>{
-  return(
-    <div className="ui container comments">
-      <div className="comment">
+const CommentDetail = ()=>{
+  return (
+    <div className="comment">
         <a href="/" className="avatar">
-          <img src="" alt="avatar"/>
+          <img src={faker.image.avatar()} alt="avatar"/>
         </a>
         <div className="content">
           <a href="/" className="author">
@@ -19,9 +16,5 @@ const App = ()=>{
           <div className="text">Nice blog post!</div>
         </div>
       </div>
-    </div>  
   );
-};
-
-ReactDOM.render(<App/>,
-  document.querySelector('#root'));
+}
