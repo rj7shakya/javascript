abf9cf9aa7446b385b4cfd9e910c06d32cec4769
rj7shakya <shakyarajad1@gmail.com>
2020-03-25 13:29:36 +0545

    pics

--- react/pics/src/components/SearchBar.js
+++ react/pics/src/components/SearchBar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
+
+  onInputChange(){
+
+  }
+
+
   render(){
     return(
       <div className="ui segment">
         <form className="ui form" action="">
           <label>Image Search</label>
-          <input type="text"/>
+          <input type="text" onChange={this.onInputChange}/>
         </form>
       </div>
     );
