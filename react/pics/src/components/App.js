0b0cadb5ea7d4bbfc284872f196ab1f00cc09cb2
rj7shakya <shakyarajad1@gmail.com>
2020-03-26 13:21:15 +0545

    api request

--- react/pics/src/components/App.js
+++ react/pics/src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term){
-    console.log(term);
+  state = {images:[]}
+
+  onSearchSubmit = async (term)=>{
+    const response = await unsplash.get('https://api.unsplash.com/search/photos',{
+      params:{ query: term}
+    });
+    
+    this.setState({images: response.data.results});
   }
 
 
@@ -11,6 +19,7 @@ class App extends React.Component {
     return(
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        <ImageList images ={this.state.images}/>
       </div>
     );
   }
