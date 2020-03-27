55b2ee09338ccff48be772b33416c5fbe7d71743
rj7shakya <shakyarajad1@gmail.com>
2020-03-27 14:08:17 +0545

    redux

---
+++ react/songs/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SongList from './SongList';
+// import {selectSong} from '../actions';
+
+
+const App = () => {
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
+}
+export default App;
--- react/yt_videos/src/api/youtube.js
+++ react/yt_videos/src/api/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'AIzaSyAkNBs9jWwFr4qbrJm194UdoB43ltc7TAg';
 
 export default axios.create({
-  baseURL:'https://www.googleapis.com/youtube/v3/',
+  baseURL:'https://www.googleapis.com/youtube/v3',
   params:{
     part:'snippet',
     maxResults: 5,
