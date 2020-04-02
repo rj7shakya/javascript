496f37d240a364a9cdeee2a42fa1e7e65262f866
rj7shakya <shakyarajad1@gmail.com>
2020-04-02 10:26:54 +0545

    git finder

---
+++ react/github-finder/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    users:[],
+    loading: false
+  }
+
+  async componentDidMount(){
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users?
+    client_id=${process.env.REACT_APP_GITHUB_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_SECRET}`)
+    this.setState({users:res.data,loading:false})
+  }
+
+  render() {
+    return (
+      <nav className='App' >
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default App;
