b4ceb71c7a824b1f2df739fc1654d63f9d7e12f9
rj7shakya <shakyarajad1@gmail.com>
2020-04-04 13:48:52 +0545

    search added

--- react/github-finder/src/App.js
+++ react/github-finder/src/App.js
@@ -2,20 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 
 class App extends Component {
   state = {
-    users:[],
+    users: [],
     loading: false
   }
 
-  async componentDidMount(){
-    this.setState({loading: true});
-    const res = await axios.get(`https://api.github.com/users?
-    client_id=${process.env.REACT_APP_GITHUB_ID}
-    &client_secret=${process.env.REACT_APP_GITHUB_SECRET}`)
-    this.setState({users:res.data,loading:false})
+
+  // //Search github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true })
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
+    // &client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}
+    console.log('adsa', res.data)
+    this.setState({ users: res.data.items, loading: false })
+    console.log(text)
+  }
+
+  clearUsers = () => {
+    console.log('Clicked')
+    // this.setState({ users: [], loading: false })
   }
 
   render() {
@@ -23,6 +32,7 @@ class App extends Component {
       <nav className='App' >
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
 
         </div>
